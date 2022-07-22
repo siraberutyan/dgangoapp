d8c49283fc2f408be0e4513214165ded8ab41efd
LAPTOP-HA9DM49F\0502_Python <murasakiyukako@gmail.com>
2022-07-22 10:01:24 +0900

    first

---
+++ hello/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Friend, Message
+
+admin.site.register(Friend)
+admin.site.register(Message)
+
---
+++ hello/forms.py
@@ -0,0 +1,55 @@
+
+from dataclasses import fields
+from tkinter.tix import Select
+from django import forms
+from.models import Friend, Message
+
+
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name','mail','gender','age','birthday']
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'mail': forms.EmailInput(attrs={'class':'form-control'}),
+            'age': forms.NumberInput(attrs={'class':'form-control'}),
+            'birthday': forms.DateInput(attrs={'class':'form-control'}),
+        }
+
+class HelloForm(forms.Form):
+    name = forms.CharField(label='Name',\
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+    mail = forms.EmailField(label='Email',\
+        widget=forms.EmailInput(attrs={'class':'form-control'}))
+    gender = forms.BooleanField(label='Gender', required=False,\
+        widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+    age = forms.IntegerField(label='Age',\
+        widget=forms.NumberInput(attrs={'class':'form-control'}))
+    birthday = forms.DateField(label='Birth',\
+        widget=forms.DateInput(attrs={'class':'form-control'}))
+
+class FindForm(forms.Form):
+    find=forms.CharField(label='Find',required=False,\
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+
+
+class CheckForm(forms.Form):
+    str = forms.CharField(label='String', \
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        str = cleaned_data['str']
+        if (str.lower().startswith('no')):
+            raise forms.ValidationError('You input "NO"!')
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['title','content','friend']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control form-control-sm'}),
+            'content': forms.Textarea(attrs={'class':'form-control form-control-sm', 'rows':2}),
+            'friend': forms.Select(attrs={'class':'form-control form-control-sm'}),
+        }
+    
